136c47383af88d348306be9183c846129ca78a1e
SaadEGI <christrophe.stephane@gmail.com>
2022-03-15 20:28:40 +0100

    Changes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('contact/', views.contact, name='contact'),
     path('home2/', views.home, name='home'),
+    path('comingsoon/', views.comingsoon, name='comingsoon'),
 
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 
 from apps.product.models import Product
+from apps.vendor.models import Vendor
 
 def frontpage(request):
     newest_products = Product.objects.all()[0:8]
+    vendorsall = Vendor.objects.all()
+    #morproducts = Product.category.filter(category='Moroccan')
 
-    return render(request, 'core/frontpage.html', {'newest_products': newest_products})
+
+
+    #category=product.category"fajsbcd"
+    #ramdproducts=category.Product.objects.all()[0:8]
+
+    return render(request, 'core/frontpage.html', {'vendorsall': vendorsall,'newest_products': newest_products})
 
 def contact(request):
     return render(request, 'core/contact.html')
 
 def home(request):
     return render(request, 'core/home2.html', {})
+
+def comingsoon(request):
+    return render(request, 'core/coming-soon.html', {})
