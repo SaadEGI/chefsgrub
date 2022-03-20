401f6d340a4e50234161ff326695a6bab52907a3
Sufyan Dahalan <sufyan.dahalan@gmail.com>
2022-03-20 02:36:23 +0100

    migrated to postgres

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -115,10 +115,20 @@ WSGI_APPLICATION = 'interiorshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CHEFSGRUB', 
+        'USER': 'strtpadmin', 
+        'PASSWORD': 'strtpadmin',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 if PRODUCTION:# or ENVIRONMENT == 'staging':
