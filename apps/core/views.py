1ccb76c7ae1f3f2d5bd0257e3bd3a6bf596ebe3f
SaadEGI <christrophe.stephane@gmail.com>
2022-03-25 12:16:19 +0100

    Updates some frontend customer link in frontpgae, ramadan products now lead to chaef page + succesful ramadan products slider

--- apps/core/views.py
+++ apps/core/views.py
@@ -6,8 +6,9 @@ from apps.vendor.models import Vendor
 def frontpage(request):
     newest_products = Product.objects.all()[0:8]
     vendorsall = Vendor.objects.all()
+    ramadan_products = Product.objects.filter(category=1)
 
-    return render(request, 'core/frontpage.html',{'newest_products': newest_products, 'vendorsall':vendorsall})
+    return render(request, 'core/frontpage.html',{'ramadan_products':ramadan_products,'newest_products': newest_products, 'vendorsall':vendorsall})
 
 def contact(request):
     return render(request, 'core/contact.html')
