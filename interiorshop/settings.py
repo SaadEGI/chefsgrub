0033b542328e0b9e65773424a9bd3a74dd254c1a
SaadEGI <christrophe.stephane@gmail.com>
2022-04-03 15:05:51 +0200

    aws key

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -74,7 +74,7 @@ AWS_STORAGE_BUCKET_NAME = 'chefsgrub'
 AWS_S_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 #AWS_S3_REGION_NAME = "ap-south-1"
-#AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'eu-central-1'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
