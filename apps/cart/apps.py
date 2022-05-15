c0d0d7757904d33cd36c50fcccc50bf5e51e8d33
SaadEGI <christrophe.stephane@gmail.com>
2022-05-15 13:45:37 +0200

    fix for no ipn received

--- apps/cart/apps.py
+++ apps/cart/apps.py
@@ -3,10 +3,8 @@ from django.apps import AppConfig
 
 class CartConfig(AppConfig):
     name = 'apps.cart'
-
-
-class CartMainConfig(AppConfig):
-    name = 'cart'
-
     def ready(self):
-        import cart.signals
+        import apps.cart.signals
+
+
+
